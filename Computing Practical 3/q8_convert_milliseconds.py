cdcc9ea574061e53cb411eaad1157835df49b7ba
flybymysky <ng.yijun@dhs.sg>
2013-05-23 12:39:04 +0800

    Update q8_convert_milliseconds.py

--- Computing Practical 3/q8_convert_milliseconds.py
+++ Computing Practical 3/q8_convert_milliseconds.py
@@ -2,6 +2,24 @@
 #author: Ng Yi Jun Alan (NALA) :D
 #note: converts milliseconds to hours, minutes, and seconds
 
+def yesno(Input): #yes no inputs
+    Y = ['yes','y']
+    N = ['no','n']
+    if Input.lower() in Y:
+        return 1
+    elif Input.lower() in N:
+        return 0
+    else:
+        Input = input("Unrecognised input. yes or no:")
+        return yesno(Input)
+
+def check_int(n):
+    try:
+        n = int(n)
+        return True
+    except ValueError:
+        return False
+
 def convert_ms(n):
     h=int( int(n) / 3600000 )
     m=int( (int(n)-h*3600000) / 60000 )
@@ -12,11 +30,13 @@ def convert_ms(n):
 #main
 print("Convert milliseconds to hours,minutes and seconds :D") 
 restart="yes"
-while restart == "yes":
+while yesno(restart) == 1:
     print()
 
-    ms= input("Enter milllisecond:")
+    ms= input("Enter millisecond:")
+    while not check_int(ms):
+        ms = input("Invalid input. millisecond:")
     print(convert_ms(ms))
 
     print()
-    restart = input("do you wish to try again? yes/no:")
+    restart = input("do you wish to try again?")
