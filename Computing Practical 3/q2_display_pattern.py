7cfe5f9f4978885c80bb637a562f52dac636ed2b
flybymysky <ng.yijun@dhs.sg>
2013-05-23 10:40:58 +0800

    Update q2_display_pattern.py

--- Computing Practical 3/q2_display_pattern.py
+++ Computing Practical 3/q2_display_pattern.py
@@ -1,19 +1,44 @@
-#filename = q2_display_pattern.py
-# author = Ng Yi Jun Alan (NALA) :D
+#filename: q1_display_reverse.py
+#author: Ng Yi Jun Alan (NALA) :D
 
-def display_pattern(n):
-    digits = []
-    for i in range(1, n+1):
-        digits.append(str(i))
-        length=len(" ".join(digits))
-    digits = []
-    for j in range(1, n+1):
-            digits.append(str(j))
-            print(" ".join(digits[::-1]).rjust(length))
-        
-#main
-integer=int(input("Input integer for pattern:"))
-display_pattern(integer)
+def yesno(Input): #yes no inputs
+    Y = ['yes','y']
+    N = ['no','n']
+    if Input.lower() in Y:
+        return 1
+    elif Input.lower() in N:
+        return 0
+    else:
+        Input = input("Unrecognised input. yes or no:")
+        return yesno(Input)
 
-print()
-print("Press enter to exit.")
+def check_int(n):
+    try:
+        n = int(n)
+        return True
+    except ValueError:
+        return False
+    
+    
+def reverse_int(n):
+    reverse = ""
+    length = len(n)-1
+    n = str(int(n)) #convert n to integer: removes any redundant zeroes at the front
+    if n[0] == "-": #check if negative number
+        reverse = "-"
+    for i in range(0, length+1):
+        if n[length-i] != "-":
+            reverse = reverse + n[length-i]
+    return(reverse)
+
+# main
+restart = "Y"
+while yesno(restart)==1:
+    
+    Input = input("Enter integer: ")
+    while not check_int(Input):
+        Input = input("Invalid input. Enter integer: ")
+    print(reverse_int(Input))
+    print()
+    
+    restart = input("do you want to try again? (yes/no)")
