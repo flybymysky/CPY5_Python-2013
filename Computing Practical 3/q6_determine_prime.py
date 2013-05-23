f677677dbf35c51eb6913882103c4563dd0c0029
flybymysky <ng.yijun@dhs.sg>
2013-05-23 12:34:13 +0800

    Update q6_determine_prime.py

--- Computing Practical 3/q6_determine_prime.py
+++ Computing Practical 3/q6_determine_prime.py
@@ -2,25 +2,21 @@
 #author: Ng Yi Jun Alan (NALA) :D
 
 def is_prime(n):
-    prime=True
     for i in range(2,n):
         if n%i ==0:
-            prime=False
-            break
-    return(prime)
+            return False
+    return True
 
 primes = []
 x=2
 while len(primes)<= 1000:
     if is_prime(x)==True:
         primes.append(str(x).ljust(4))
-        x=x+1
-    else:
-        x=x+1
+    x=x+1
 
-a=0
-b=10
-while b<=1000:
-    print(" ".join(primes[a:b]))
-    a=a+10
-    b=b+10
+start=0
+end=10
+while end<=1000:
+    print(" ".join(primes[start:end]))
+    start=start+10
+    end=end+10
